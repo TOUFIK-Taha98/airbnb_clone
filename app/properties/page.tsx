e7f4fa6f86d7947899cd0acda978542f7e36d068
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2023-12-22 16:20:26 +0100

    Properties screen (Loading your listings with server component)

---
+++ app/properties/page.tsx
@@ -0,0 +1,41 @@
+import EmptyState from "../components/EmtyState";
+import ClientOnly from "@/app/components/ClientOnly";
+
+import getCurrentUser from "@/app/actions/getCurrentUser";
+
+import TripsClient from "./PropertiesClient";
+import getListings from "../actions/getListings";
+import PropertiesClient from "./PropertiesClient";
+
+const PropertiesPage = async () => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return (
+      <ClientOnly>
+        <EmptyState title="Unauthorized" subtitle="Please login" />
+      </ClientOnly>
+    );
+  }
+
+  const listings = await getListings({ userId: currentUser.id });
+
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState
+          title="No reservations found"
+          subtitle="Looks like you have no properties available"
+        />
+      </ClientOnly>
+    );
+  }
+
+  return (
+    <ClientOnly>
+      <PropertiesClient listings={listings} currentUser={currentUser} />
+    </ClientOnly>
+  );
+};
+
+export default PropertiesPage;
