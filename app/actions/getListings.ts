c05bf9318ae918e2005f33ffc1cb86e08d51e3c0
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2023-12-25 11:44:52 +0100

    Filters modal (Assigning various filters, add advanced querying logic to getListings, add loading and error pages)

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -4,16 +4,77 @@ import prisma from "@/app/libs/prismaDb";
 
 export interface IListingsParams {
   userId?: string;
+  guestcount?: number;
+  roomCount?: number;
+  bathRoomCount?: number;
+  startDate?: string;
+  endDate?: string;
+  locationValue?: string;
+  category?: string;
 }
 
 export default async function getListings(params: IListingsParams) {
   try {
-    const { userId } = params;
+    const {
+      userId,
+      guestcount,
+      roomCount,
+      bathRoomCount,
+      startDate,
+      endDate,
+      locationValue,
+      category,
+    } = params;
 
     let query: any = {};
 
     if (userId) {
       query.userId = userId;
+    }
+
+    if (category) {
+      query.category = category;
+    }
+
+    if (roomCount) {
+      query.roomCount = {
+        gte: +roomCount, // transform string to definite number
+      };
+    }
+
+    if (guestcount) {
+      query.guestcount = {
+        gte: +guestcount, // transform string to definite number
+      };
+    }
+
+    if (bathRoomCount) {
+      query.bathRoomCount = {
+        gte: +bathRoomCount, // transform string to definite number
+      };
+    }
+
+    if (locationValue) {
+      query.locationValue = locationValue;
+    }
+
+    if (startDate && endDate) {
+      query.NOT = {
+        reservations: {
+          some: {
+            OR: [
+              {
+                endDate: { gte: startDate },
+                startDate: { lte: startDate },
+              },
+              {
+                startDate: { lte: startDate },
+                endDate: { gte: endDate },
+              },
+            ],
+          },
+        },
+      };
     }
 
     const listings = await prisma.listing.findMany({
--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ import RentModal from "./components/modals/RentModal";
 
 import ToasterProvider from "./providers/ToasterProvider";
 import getCurrentUser from "./actions/getCurrentUser";
+import SearchModal from "./components/modals/SearchModal";
 
 export const metadata: Metadata = {
   title: "Airbnb",
@@ -32,9 +33,10 @@ export default async function RootLayout({
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
+          <SearchModal />
+          <RentModal />
           <LoginModal />
           <RegisterModal />
-          <RentModal />
           <Navbar currentUser={currentUser} />
         </ClientOnly>
         <div className="pb-20 pt-28">{children}</div>
